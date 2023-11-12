a4fe3984cbf8427ceacc763ca0c388e646b2dc11
Walter Tronics <waltertronics02@gmail.com>
2023-11-12 17:42:24 +0100

    set base code

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,25 @@ function App() {
 
   return (
     <>
-      <div>
-        
-      </div>
+      <section>
+        <h1>Random Quote</h1>
+
+        {/* Quote content */}
+        <blockquote id='quote'>
+
+        </blockquote>
+
+        {/* Author of the quote */}
+        <div id='author'>
+
+        </div>
+
+        {/* Query for quote */}
+        <button>Get Quote</button>
+
+        {/* Acknowledgement  */}
+        <h5>Quotes acquired from <a href="https://www.goodreads.com/quotes/tag/inspirational">Goodreads</a></h5>
+      </section>
     </>
   )
 }
