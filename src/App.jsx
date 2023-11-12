5aeda6052e0b9d2025aaaa78088f10826d3079b6
Walter Tronics <waltertronics02@gmail.com>
2023-11-12 18:18:11 +0100

    import the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
