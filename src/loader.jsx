3826f69ef6d2d8f287d7c854f5e00f94c531f32a
Walter Tronics <waltertronics02@gmail.com>
2023-12-23 19:22:43 +0100

    add the basic loader html content

---
+++ src/loader.jsx
@@ -0,0 +1,63 @@
+import './loader.css';
+
+
+const Loader = () => {
+
+
+    return (
+        <div className='load_cont'>
+            <div class="hexagon" aria-label="Animated hexagonal ripples">
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+                <div class="hexagon__group">
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                    <div class="hexagon__sector"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
